72c5ae723491a0c64e4922fff90c14059b6c9e8c
Vanessa <lly219@gmail.com>
2022-05-19 15:02:19 +0800

    :bug: iOS 端关于页未显示 IP 地址 Fix https://github.com/siyuan-note/siyuan/issues/4949

--- siyuan-ios/ViewController.swift
+++ siyuan-ios/ViewController.swift
@@ -136,7 +136,7 @@ class ViewController: UIViewController, WKNavigationDelegate, UIScrollViewDelega
         Iosk.MobileSetDefaultLang("zh_CN")
         let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         let documentsDirectory = urls[0]
-        Iosk.MobileStartKernel("ios", Bundle.main.resourcePath, documentsDirectory.path, "", "", TimeZone.current.identifier, "");
+        Iosk.MobileStartKernel("ios", Bundle.main.resourcePath, documentsDirectory.path, "", "", TimeZone.current.identifier, getIP());
     }
     
     func waitFotKernelHttpServing() {
@@ -148,6 +148,35 @@ class ViewController: UIViewController, WKNavigationDelegate, UIScrollViewDelega
         }
      }
     
+    func getIP() -> String? {
+        var address: String?
+        var ifaddr: UnsafeMutablePointer<ifaddrs>?
+        if getifaddrs(&ifaddr) == 0 {
+            var ptr = ifaddr
+            while ptr != nil {
+                defer { ptr = ptr?.pointee.ifa_next } // memory has been renamed to pointee in swift 3 so changed memory to pointee
+                guard let interface = ptr?.pointee else {
+                    return nil
+                }
+                let addrFamily = interface.ifa_addr.pointee.sa_family
+                if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                    guard let ifa_name = interface.ifa_name else {
+                        return nil
+                    }
+                    let name: String = String(cString: ifa_name)
+                    
+                    if name == "en0" {  // String.fromCString() is deprecated in Swift 3. So use the following code inorder to get the exact IP Address.
+                        var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                        getnameinfo(interface.ifa_addr, socklen_t((interface.ifa_addr.pointee.sa_len)), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
+                        address = String(cString: hostname)
+                    }
+                }
+            }
+            freeifaddrs(ifaddr)
+        }
+        return address
+    }
+
     @objc func keyboardDidHide(notification: NSNotification) {
         keyboardShowed = true
         if syWebView.frame.size.height != view.safeAreaLayoutGuide.layoutFrame.height {
