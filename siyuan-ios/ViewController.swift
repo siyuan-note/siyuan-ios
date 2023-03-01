ebb217ccee796113546ad120faafb71a7d0f6df1
Vanessa <lly219@gmail.com>
2023-03-01 10:59:29 +0800

    :iphone: fix https://github.com/siyuan-note/siyuan/issues/7532

--- siyuan-ios/ViewController.swift
+++ siyuan-ios/ViewController.swift
@@ -41,6 +41,7 @@ class ViewController: UIViewController, WKNavigationDelegate, UIScrollViewDelega
         }
         
         initKernel()
+        ViewController.syWebView.hack_removeInputAccessory();
         
         // js 中调用 swift
         ViewController.syWebView.configuration.defaultWebpagePreferences.allowsContentJavaScript = true
@@ -267,5 +268,36 @@ extension UIColor {
         Scanner(string: fullHex)
             .scanHexInt64(&hexComponent)
         return CGFloat(Double(hexComponent) / 255.0)
+    }
+}
+
+fileprivate final class InputAccessoryHackHelper: NSObject {
+    @objc var inputAccessoryView: AnyObject? { return nil }
+}
+
+extension WKWebView {
+    func hack_removeInputAccessory() {
+        guard let target = scrollView.subviews.first(where: {
+            String(describing: type(of: $0)).hasPrefix("WKContent")
+        }), let superclass = target.superclass else {
+            return
+        }
+
+        let noInputAccessoryViewClassName = "\(superclass)_NoInputAccessoryView"
+        var newClass: AnyClass? = NSClassFromString(noInputAccessoryViewClassName)
+
+        if newClass == nil, let targetClass = object_getClass(target), let classNameCString = noInputAccessoryViewClassName.cString(using: .ascii) {
+            newClass = objc_allocateClassPair(targetClass, classNameCString, 0)
+
+            if let newClass = newClass {
+                objc_registerClassPair(newClass)
+            }
+        }
+
+        guard let noInputAccessoryClass = newClass, let originalMethod = class_getInstanceMethod(InputAccessoryHackHelper.self, #selector(getter: InputAccessoryHackHelper.inputAccessoryView)) else {
+            return
+        }
+        class_addMethod(noInputAccessoryClass.self, #selector(getter: InputAccessoryHackHelper.inputAccessoryView), method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+        object_setClass(target, noInputAccessoryClass)
     }
 }
