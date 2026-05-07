f804e25593a84262d9f77742d4e850573aa14677
Vanessa <lly219@gmail.com>
2026-05-07 20:12:15 +0800

    :art: https://github.com/siyuan-note/siyuan/issues/17625

--- siyuan-ios/SceneDelegate.swift
+++ siyuan-ios/SceneDelegate.swift
@@ -70,6 +70,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        ViewController.syWebView.evaluateJavaScript("lockscreenByMode();")
     }
 
 
